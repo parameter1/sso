fc12b78d1049a69c26924ed066a5281c55b1c75f
Jacob Bare <jacob@parameter1.com>
2022-01-20 11:35:09 -0600

    Add `OrganizationRepo.create` method

--- packages/db/src/repo/organization.js
+++ packages/db/src/repo/organization.js
@@ -1,6 +1,12 @@
-import { ManagedRepo } from '@parameter1/mongodb';
+import { ManagedRepo, cleanDocument } from '@parameter1/mongodb';
+import Joi, { validateAsync } from '@parameter1/joi';
+import { organizationAttributes as attrs } from '../schema/attributes/index.js';
 
 export default class OrganizationRepo extends ManagedRepo {
+  /**
+   *
+   * @param {object} params
+   */
   constructor(params = {}) {
     super({
       ...params,
@@ -13,6 +19,34 @@ export default class OrganizationRepo extends ManagedRepo {
         { key: { 'date.created': 1, _id: 1 } },
         { key: { 'date.updated': 1, _id: 1 } },
       ],
+    });
+  }
+
+  /**
+   * @param {object} params
+   * @param {string} params.name
+   * @param {string} params.slug
+   * @param {object} [params.options]
+   */
+  async create(params = {}) {
+    const {
+      name,
+      slug,
+      options,
+    } = await validateAsync(Joi.object({
+      name: attrs.name.required(),
+      slug: attrs.slug.required(),
+      options: Joi.object().default({}),
+    }).required(), params);
+
+    const now = new Date();
+    return this.insertOne({
+      doc: cleanDocument({
+        name,
+        slug,
+        date: { created: now, updated: now },
+      }),
+      options,
     });
   }
 }

dcaf6a8ed935ca4ba9c7dbbac0d5dc010174735c
Jacob Bare <jacob@parameter1.com>
2022-01-25 10:36:16 -0600

    Handle when no applications exist

--- cli/src/actions/application/update-name.js
+++ cli/src/actions/application/update-name.js
@@ -2,22 +2,33 @@ import inquirer from 'inquirer';
 import { applicationAttributes as appAttrs } from '@parameter1/sso-db/schema';
 import { getAppList } from '../utils/index.js';
 import repos from '../../repos.js';
+import create from './create.js';
 
 const { log } = console;
 
 export default async function createInstance() {
+  const appList = await getAppList();
   const questions = [
     {
       type: 'list',
       name: 'app',
+      when: Boolean(appList.length),
       message: 'Select the application',
-      choices: getAppList,
+      choices: appList,
+    },
+    {
+      type: 'confirm',
+      name: 'createApp',
+      when: Boolean(!appList.length),
+      message: 'No applications exit. Would you like to create one?',
+      default: false,
     },
     {
       type: 'input',
       name: 'name',
       default: ({ app }) => app.name,
       message: 'Enter the new application name',
+      when: ({ app }) => Boolean(app),
       validate: async (input) => {
         const { error } = appAttrs.name.required().validate(input);
         if (error) return error;
@@ -29,20 +40,26 @@ export default async function createInstance() {
       name: 'confirm',
       message: 'Are you sure you want to complete this action?',
       default: false,
+      when: ({ app }) => Boolean(app),
     },
   ];
 
   const {
+    createApp,
     confirm,
     app,
     name,
   } = await inquirer.prompt(questions);
 
-  if (!confirm) return;
+  if (createApp) {
+    return create();
+  }
+
+  if (!confirm || !app) return null;
 
   const result = await repos.$('application').updateName({
     id: app._id,
     name,
   });
-  log(result);
+  return log(result);
 }

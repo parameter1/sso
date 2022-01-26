e6366da932d39e81c6f2c0d3e4da318464fe5125
Jacob Bare <jacob@parameter1.com>
2022-01-26 15:11:30 -0600

    Create error and alert elements

---
+++ app/src/components/alert.vue
@@ -0,0 +1,25 @@
+<template>
+  <div role="alert">
+    <slot name="header">
+      <h3>
+        {{ header }}
+      </h3>
+    </slot>
+    <p>
+      <slot />
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AlertElement',
+
+  props: {
+    header: {
+      type: String,
+      default: null,
+    },
+  },
+};
+</script>
---
+++ app/src/components/error.vue
@@ -0,0 +1,29 @@
+<template>
+  <alert-element v-if="error" :header="header">
+    {{ error }}
+  </alert-element>
+</template>
+
+<script>
+import AlertElement from './alert.vue';
+
+export default {
+  name: 'ErrorAlert',
+
+  components: {
+    AlertElement,
+  },
+
+  props: {
+    error: {
+      type: Error,
+      default: null,
+    },
+
+    header: {
+      type: String,
+      default: 'Error',
+    },
+  },
+};
+</script>

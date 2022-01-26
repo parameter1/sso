d7b586618d0d83cd8e45396043e30e83a6ea0fd3
Jacob Bare <jacob@parameter1.com>
2022-01-26 15:20:46 -0600

    Create button component

---
+++ app/src/components/button.vue
@@ -0,0 +1,35 @@
+<template>
+  <button
+    :type="type"
+    :disabled="disabled || loading"
+    @click="$emit('click', $event)"
+  >
+    <span v-if="loading">
+      Loading...
+    </span>
+    <slot v-else />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'ButtonElement',
+  emits: ['click'],
+
+  props: {
+    type: {
+      type: String,
+      default: 'button',
+      validator: (value) => ['button', 'submit', 'reset'].includes(value),
+    },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>

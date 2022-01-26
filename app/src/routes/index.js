84012ae004199a58465085fc01c6b90d6972564c
Jacob Bare <jacob@parameter1.com>
2022-01-26 16:27:12 -0600

    Create error and not found pages

--- app/src/routes/index.js
+++ app/src/routes/index.js
@@ -11,6 +11,17 @@ const routes = [
     name: 'style-guide',
     component: () => import('../pages/style-guide.vue'),
   },
+  {
+    path: '/error',
+    name: 'error',
+    component: () => import('../pages/error.vue'),
+    props: true,
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'not-found',
+    component: () => import('../pages/not-found.vue'),
+  },
 ];
 
 const router = createRouter({

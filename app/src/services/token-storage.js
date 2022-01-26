045de87bb48fa50d9fb4a09b243a77d8be8aec04
Jacob Bare <jacob@parameter1.com>
2022-01-26 16:23:12 -0600

    Save token values as objects

--- app/src/services/token-storage.js
+++ app/src/services/token-storage.js
@@ -3,14 +3,22 @@ import constants from '../constants';
 
 const { TOKEN_KEY } = constants;
 
+const parse = (json) => {
+  try {
+    return JSON.parse(json);
+  } catch (e) {
+    return null;
+  }
+};
+
 const setCookie = (value) => cookie.set(TOKEN_KEY, value, { expires: 86400 });
 
 const get = () => {
-  const storageValue = localStorage.getItem(TOKEN_KEY);
-  const cookieValue = cookie.get(TOKEN_KEY);
+  const storageValue = parse(localStorage.getItem(TOKEN_KEY));
+  const cookieValue = parse(cookie.get(TOKEN_KEY));
 
   // restore cookie if missing and storage value is present
-  if (!cookieValue && storageValue) setCookie(storageValue);
+  if (!cookieValue && storageValue) setCookie(JSON.stringify(storageValue));
   // remove cookie if present but storage value is missing
   if (cookieValue && !storageValue) cookie.remove(TOKEN_KEY);
   return storageValue;
@@ -23,9 +31,10 @@ export default {
     localStorage.removeItem(TOKEN_KEY);
     cookie.remove(TOKEN_KEY);
   },
-  set: (value) => {
-    if (!value) throw new Error('Unable to set auth token: no value was provided.');
-    localStorage.setItem(TOKEN_KEY, value);
-    setCookie(value);
+  set: (obj) => {
+    if (!obj || !obj.value) throw new Error('Unable to set token: no value was provided.');
+    const json = JSON.stringify(obj);
+    localStorage.setItem(TOKEN_KEY, json);
+    setCookie(json);
   },
 };

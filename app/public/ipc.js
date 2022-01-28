eb3a70501da4ac5c58852ee092bb21f6d22a2f80
Jacob Bare <jacob@parameter1.com>
2022-01-28 13:57:37 -0600

    Add ipc token init handling

---
+++ app/public/ipc.js
@@ -0,0 +1,57 @@
+const parseJSON = (json) => {
+  try {
+    return JSON.parse(json);
+  } catch (e) {
+    return null;
+  }
+};
+
+const sendMessage = ({ key, action, value }) => {
+  const message = { key, action, value };
+  // @todo must find a way to whitelist the message domains!
+  window.parent.postMessage(JSON.stringify(message), '*');
+};
+
+const keys = {
+  token: '__p1-sso-token',
+};
+
+const init = () => {
+  const { token: key } = keys;
+  const value = parseJSON(localStorage.getItem(key));
+  sendMessage({ key, action: 'init', value });
+};
+
+const sendAdd = (value) => {
+  const { token: key } = keys;
+  sendMessage({ key, action: 'add', value });
+};
+
+const sendRemove = () => {
+  const { token: key } = keys;
+  sendMessage({ key, action: 'remove' });
+};
+
+init();
+
+window.addEventListener('storage', (event) => {
+  const { key, newValue } = event;
+  // if key is null, all of local storage was cleared. signal a token removal.
+  if (key === null) {
+    sendRemove();
+    return;
+  }
+  // only act on the token key...
+  if (key === keys.token) {
+    // if a new value is set. singal the token addition if parsable
+    if (newValue) {
+      const parsed = parseJSON(newValue);
+      if (parsed) {
+        sendAdd(parsed);
+      }
+      return;
+    }
+    // otherwise treat as a clear/logout
+    sendRemove();
+  }
+});

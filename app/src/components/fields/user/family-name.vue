5a32cd65100b31e2eb10a786804290b2b0d668de
Jacob Bare <jacob@parameter1.com>
2022-01-30 10:13:37 -0600

    Create user given and family name field components

---
+++ app/src/components/fields/user/family-name.vue
@@ -0,0 +1,60 @@
+<template>
+  <field-group
+    type="text"
+    autocomplete="family-name"
+    :id="id"
+    :label="label"
+    :index="index"
+    :required="required"
+    :disabled="disabled"
+    :model-value="modelValue"
+    @did-change="$emit('didChange', $event)"
+    @update:model-value="$emit('update:modelValue', $event)"
+  />
+</template>
+
+<script>
+import FieldGroup from '../group.vue';
+
+export default {
+  name: 'UserEmailField',
+
+  emits: ['didChange', 'update:modelValue'],
+
+  components: {
+    FieldGroup,
+  },
+
+  props: {
+    id: {
+      type: String,
+      default: 'user-family-name',
+    },
+
+    label: {
+      type: String,
+      default: 'Last Name',
+    },
+
+    modelValue: {
+      type: String,
+      default: null,
+    },
+
+    required: {
+      type: Boolean,
+      default: true,
+    },
+
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+
+    index: {
+      type: Number,
+      default: null,
+    },
+  },
+};
+</script>
---
+++ app/src/components/fields/user/given-name.vue
@@ -0,0 +1,60 @@
+<template>
+  <field-group
+    type="text"
+    autocomplete="given-name"
+    :id="id"
+    :label="label"
+    :index="index"
+    :required="required"
+    :disabled="disabled"
+    :model-value="modelValue"
+    @did-change="$emit('didChange', $event)"
+    @update:model-value="$emit('update:modelValue', $event)"
+  />
+</template>
+
+<script>
+import FieldGroup from '../group.vue';
+
+export default {
+  name: 'UserEmailField',
+
+  emits: ['didChange', 'update:modelValue'],
+
+  components: {
+    FieldGroup,
+  },
+
+  props: {
+    id: {
+      type: String,
+      default: 'user-given-name',
+    },
+
+    label: {
+      type: String,
+      default: 'First Name',
+    },
+
+    modelValue: {
+      type: String,
+      default: null,
+    },
+
+    required: {
+      type: Boolean,
+      default: true,
+    },
+
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+
+    index: {
+      type: Number,
+      default: null,
+    },
+  },
+};
+</script>

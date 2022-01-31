ad0cc602f71f73f219e526760dae3b3eca6f4fe7
Jacob Bare <jacob@parameter1.com>
2022-01-31 14:27:11 -0600

    Create node api/graphql client package

---
+++ packages/node-api-client/README.md
@@ -0,0 +1,1 @@
+# Node SSO GraphQL/API Client
---
+++ packages/node-api-client/src/index.js
@@ -0,0 +1,3 @@
+export { default as GraphQLError } from './error.js';
+export { default as GraphQLClient } from './graphql.js';
+export { default as WorkspaceClient } from './workspace.js';

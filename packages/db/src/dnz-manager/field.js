d3ddfdf773a738b94bdd8dae7c2e3a91ea65a145
Jacob Bare <jacob@parameter1.com>
2022-02-05 17:47:30 -0600

    Prevent `_id` field definitions

--- packages/db/src/dnz-manager/field.js
+++ packages/db/src/dnz-manager/field.js
@@ -2,6 +2,7 @@ export default class DenormalizedField {
   constructor({ name, schema } = {}) {
     if (!name) throw new Error('The denormalized field name must be provided.');
     if (!schema) throw new Error('No Joi schema was provided for this field.');
+    if (name === '_id') throw new Error('You cannot define/manipulate the denormalized _id field.');
     this.name = name;
     this.schema = schema;
   }

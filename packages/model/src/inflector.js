ba4be7254368a26751f7cccca45500d8f536ca5f
Jacob Bare <jacob@parameter1.com>
2022-02-07 17:27:44 -0600

    Use `change-case` packages in inflector

--- packages/model/src/inflector.js
+++ packages/model/src/inflector.js
@@ -1,19 +1,14 @@
 import infl from 'inflected';
-import { ucFirst } from '@parameter1/utils';
-import is from '@sindresorhus/is';
-
-const classify = (value) => infl.classify(infl.underscore(value));
+import { paramCase } from 'param-case';
+import { pascalCase } from 'pascal-case';
+import { camelCase } from 'camel-case';
+import { noCase } from 'no-case';
 
 export default {
-  classify: (value) => {
-    if (!is.string(value)) throw new Error('The value must be a string');
-    const parts = value.trim().split(' ');
-    if (parts.length === 1) return classify(parts[0]);
-    const cleaned = parts
-      .map((p) => p.trim())
-      .filter((p) => p)
-      .map((p) => ucFirst(p.toLowerCase()))
-      .join('');
-    return classify(cleaned);
-  },
+  camel: (value) => camelCase(value),
+  none: (value) => noCase(value),
+  pascal: (value) => pascalCase(value),
+  param: (value) => paramCase(value),
+  plural: (value) => infl.pluralize(value),
+  singular: (value) => infl.singularize(value),
 };
--- packages/model/test/inflector.spec.js
+++ packages/model/test/inflector.spec.js
@@ -3,29 +3,67 @@ import { describe, it } from 'mocha';
 import { expect } from 'chai';
 import inflector from '../src/inflector.js';
 
-describe('utils/inflector.js', () => {
-  describe('classify', () => {
-    it('should throw an error when a string is not provided.');
+const {
+  camel,
+  none,
+  pascal,
+  param,
+} = inflector;
+
+const words = [
+  'UserEvent',
+  ' UserEvent ',
+  'user event',
+  'User Event',
+  'User event',
+  'user Event',
+  'User   event  ',
+  'user_event',
+  '_user_event',
+  'user__event',
+  ' user_event ',
+  'user-event',
+  '-user-event',
+  'user--event',
+  'userEvent',
+];
+
+describe('inflector.js', () => {
+  describe('pascal', () => {
     describe('multiple words', () => {
-      [
-        'UserEvent',
-        ' UserEvent ',
-        'user event',
-        'User Event',
-        'User event',
-        'user Event',
-        'User   event  ',
-        'user_event',
-        '_user_event',
-        'user__event',
-        ' user_event ',
-        'user-event',
-        '-user-event',
-        'user--event',
-        'userEvent',
-      ].forEach((value) => {
+      words.forEach((value) => {
         it(`should handle ${value}`, () => {
-          expect(inflector.classify(value)).to.equal('UserEvent');
+          expect(pascal(value)).to.equal('UserEvent');
+        });
+      });
+    });
+  });
+
+  describe('param', () => {
+    describe('multiple words', () => {
+      words.forEach((value) => {
+        it(`should handle ${value}`, () => {
+          expect(param(value)).to.equal('user-event');
+        });
+      });
+    });
+  });
+
+  describe('camel', () => {
+    describe('multiple words', () => {
+      words.forEach((value) => {
+        it(`should handle ${value}`, () => {
+          expect(camel(value)).to.equal('userEvent');
+        });
+      });
+    });
+  });
+
+  describe('none', () => {
+    describe('multiple words', () => {
+      words.forEach((value) => {
+        it(`should handle ${value}`, () => {
+          expect(none(value)).to.equal('user event');
         });
       });
     });

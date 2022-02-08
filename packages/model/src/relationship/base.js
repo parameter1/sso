e439de9a48f23da596f1d05d0d976e017459365d
Jacob Bare <jacob@parameter1.com>
2022-02-08 11:09:22 -0600

    Add `Relationship.entity` docblock

--- packages/model/src/relationship/base.js
+++ packages/model/src/relationship/base.js
@@ -44,6 +44,30 @@ export default class Relationship extends Base {
     return this.$set('as', camel(value));
   }
 
+  /**
+   * Sets the related entity. The value will be converted to PascalCase in
+   * singular form (e.g. `fruit-snacks` would become `FruitSnack`) - just as the
+   * `Entity.name` value is handled.
+   *
+   * The `type`, method must before called before calling the `entity` method,
+   * otherwise an error will be thrown.
+   *
+   * This method is called automatically when the relationship `one` or `many`
+   * helper functions are invoked.
+   *
+   * ```
+   * // in both cases the internal entity name is stored as `Foo`
+   * rel.type('one').entity('Foo');
+   * rel.type('many').entity('Foos');
+   *
+   * // using the `one` or `many` functions
+   * one('Foo');
+   * many('Foos');
+   * ```
+   *
+   * @param {string} value The related entity name
+   * @returns {Relationship}
+   */
   entity(value) {
     this.$needs('type');
     return this.$set('entity', entityName(value), { strict: true });

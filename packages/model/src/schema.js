19f3d9fd004aaa0236b4232039a3d81a5caad1a6
Jacob Bare <jacob@parameter1.com>
2022-02-07 22:14:40 -0600

    Export any schema

--- packages/model/src/schema.js
+++ packages/model/src/schema.js
@@ -1,8 +1,9 @@
 import Joi from '@parameter1/joi';
 
 // types
-export const string = () => Joi.string();
+export const any = () => Joi.any();
 export const array = () => Joi.array();
+export const string = () => Joi.string();
 
 // utils
 export const { attempt, isSchema } = Joi;

4c7cea20ffbc0c586c883392e87e1625e8703d33
Jacob Bare <jacob@parameter1.com>
2022-02-07 22:54:43 -0600

    Add `Entity.prop` method

--- packages/model/src/entity.js
+++ packages/model/src/entity.js
@@ -1,8 +1,14 @@
 import Base from './base.js';
 import inflector from './inflector.js';
 import entityName from './utils/entity-name.js';
+import {
+  attempt,
+  isSchema,
+  object,
+  string,
+} from './schema.js';
 
-const { param, plural } = inflector;
+const { camel, param, plural } = inflector;
 
 class Entity extends Base {
   /**
@@ -20,6 +26,30 @@ class Entity extends Base {
       .$set('plural', plural(name));
   }
 
+  /**
+   * Defines a property on this entity, along with its schema. The property
+   * name will be automatically converted to camelCase (e.g. `pull_request` would
+   * become `pullRequest`).
+   *
+   * Will throw an error if an existing property is already set.
+   *
+   * ```
+   * entity('foo')
+   *  .prop('bar', string())
+   *  .prop('baz', boolean());
+   * ```
+   *
+   * @param {string} name The name of the property.
+   * @param {object} schema The schema to use when validating the property's value
+   * @returns {Entity} The cloned instance
+   */
+  prop(name, schema) {
+    const k = attempt(name, string().required());
+    if (!isSchema(schema)) throw Error('A Joi schema is required when setting a prop.');
+    const path = `props.${camel(k)}`;
+    return this.$set(path, { schema }, { schema: object(), strict: true });
+  }
+
   collection(value) {
     return this.$set('collection', value);
   }
@@ -32,6 +62,12 @@ class Entity extends Base {
 
 const o = new Entity();
 
+/**
+ * Creates a new entity definition.
+ *
+ * @param {string} name The entity name.
+ * @returns {Entity}
+ */
 export default function entity(name) {
   return o.name(name);
 }
--- packages/model/test/entity.spec.js
+++ packages/model/test/entity.spec.js
@@ -2,6 +2,8 @@
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
 import entity from '../src/entity.js';
+import { string, isSchema } from '../src/schema.js';
+import common from './common.js';
 
 describe('entity.js', () => {
   it('should pluralize and dasherize the collection name by default', () => {
@@ -22,6 +24,36 @@ describe('entity.js', () => {
         const ent = entity(name);
         expect(ent.$get('plural')).to.equal('UserEvents');
       });
+    });
+  });
+
+  describe('prop', () => {
+    it('should throw an error when the key is invalid', () => {
+      common.testInvalidRequiredStrings((value) => {
+        entity('foo').prop(value);
+      });
+    });
+    it('should throw an error when an existing prop is already set', () => {
+      expect(() => {
+        entity('foo').prop('bar', string()).prop('bar', string());
+      }).to.throw(Error, 'A value already exists for `props.bar`');
+    });
+    it('should throw an error when a Joi schema is omitted', () => {
+      [undefined, null, {}, string].forEach((schema) => {
+        expect(() => {
+          entity('foo').prop('bar', schema);
+        }).to.throw(Error, 'A Joi schema is required when setting a prop.');
+      });
+    });
+    it('should camelize the prop name', () => {
+      ['foo_bar', 'FooBar', 'foo-bar', 'foo bar', 'foo.bar', 'foo__bar'].forEach((name) => {
+        const v = entity('foo').prop(name, string()).$has('props.fooBar');
+        expect(v).to.equal(true);
+      });
+    });
+    it('should set the schema', () => {
+      const prop = entity('foo').prop('foo', string()).$get('props.foo');
+      expect(isSchema(prop.schema)).to.equal(true);
     });
   });
 });

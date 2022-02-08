50692b959d9f2fd00b73f1b18f999db41af0f095
Jacob Bare <jacob@parameter1.com>
2022-02-08 00:18:36 -0600

    Export all of joi + alternatives + conditional

--- packages/model/src/schema.js
+++ packages/model/src/schema.js
@@ -1,10 +1,14 @@
 import Joi from '@parameter1/joi';
 
 // types
+export const alternatives = () => Joi.alternatives();
 export const any = () => Joi.any();
 export const array = () => Joi.array();
+export const conditional = (...args) => Joi.alternatives().conditional(...args);
 export const object = () => Joi.object();
 export const string = () => Joi.string();
 
 // utils
 export const { attempt, isSchema } = Joi;
+
+export default Joi;

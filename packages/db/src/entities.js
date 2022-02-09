e7b0e79189d72196c194d1816cebb16c1e67d02b
Jacob Bare <jacob@parameter1.com>
2022-02-09 09:41:37 -0600

    Update entities example

--- packages/db/src/entities.js
+++ packages/db/src/entities.js
@@ -1,58 +1,66 @@
-import Joi from '@parameter1/joi';
 import { isEmailBurner } from '@parameter1/email-utils';
 import { cleanPath } from '@parameter1/utils';
-import { entity } from '@parameter1/sso-model';
+import {
+  array,
+  boolean,
+  date,
+  entity,
+  email,
+  hostname,
+  ipv4,
+  object,
+  sequence,
+  slug,
+  string,
+  url,
+} from '@parameter1/sso-model';
 import environments from './schema/environments.js';
 
 const common = {
-  email: Joi.email().lowercase().custom((value, helpers) => {
+  email: email().lowercase().custom((value, helpers) => {
     if (isEmailBurner(value)) return helpers.error('string.email');
     return value;
   }),
-  emailDomain: Joi.hostname(),
-  name: Joi.string().min(2),
-  slug: Joi.slug().min(2),
-  ipv4: Joi.string().ip({ version: ['ipv4'], cidr: 'forbidden' }),
+  emailDomain: hostname(),
+  name: string().min(2),
+  slug: slug().min(2),
 };
 
 export default [
-  entity('Application').props([
-    { name: 'name', schema: common.name },
-    { name: 'redirects', schema: Joi.array().items(common.slug) },
-    { name: 'roles', schema: Joi.array().items(Joi.string()) },
-    { name: 'slug', schema: common.slug },
-  ]),
-  entity('Organization').props([
-    { name: 'emailDomains', schema: Joi.array().items(common.emailDomain) },
-    { name: 'name', schema: common.name },
-    { name: 'redirects', schema: Joi.array().items(common.slug) },
-    { name: 'slug', schema: common.slug },
-  ]),
-  entity('User').props([
-    { name: 'domain', schema: common.emailDomain },
-    { name: 'email', schema: common.email },
-    { name: 'familyName', schema: Joi.string() },
-    { name: 'givenName', schema: Joi.string() },
-    { name: 'loginCount', schema: Joi.sequence() },
-    { name: 'previousEmails', schema: Joi.array().items(common.emailDomain) },
-    { name: 'verified', schema: Joi.boolean() },
-  ]),
-  entity('UserEvent').props([
-    { name: 'action', schema: Joi.string().valid('magic-login', 'send-login-link', 'logout') },
-    { name: 'data', schema: Joi.object().unknown() },
-    { name: 'date', schema: Joi.date() },
-    { name: 'ip', schema: common.ipv4 },
-    { name: 'ua', schema: Joi.string() },
-  ]),
-  entity('Workspace').props([
-    { name: 'name', schema: common.name },
-    { name: 'redirects', schema: Joi.array().items(common.slug) },
-    { name: 'slug', schema: common.slug },
-    {
-      name: 'urls',
-      schema: environments.reduce((o, env) => ({
-        ...o, [env]: Joi.url().custom(cleanPath),
-      }), {}),
-    },
-  ]),
+  entity('Application').props({
+    name: common.name,
+    redirects: array().items(common.slug),
+    roles: array().items(string()),
+    slug: common.slug,
+  }),
+  entity('Organization').props({
+    emailDomains: array().items(common.emailDomain),
+    name: common.name,
+    redirects: array().items(common.slug),
+    slug: common.slug,
+  }),
+  entity('User').props({
+    domain: common.emailDomain,
+    email: common.email,
+    familyName: string(),
+    givenName: string(),
+    loginCount: sequence(),
+    previousEmails: array().items(common.emailDomain),
+    verified: boolean(),
+  }),
+  entity('UserEvent').props({
+    action: string().valid('magic-login', 'send-login-link', 'logout'),
+    data: object().unknown(),
+    date: date(),
+    ip: ipv4(),
+    ua: string(),
+  }),
+  entity('Workspace').props({
+    name: common.name,
+    redirects: array().items(common.slug),
+    slug: common.slug,
+    urls: environments.reduce((o, env) => ({
+      ...o, [env]: url().custom(cleanPath),
+    }), {}),
+  }),
 ];

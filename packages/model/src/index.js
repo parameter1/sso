fb3dd426eb8c44d518f6b919483ca86340d6f159
Jacob Bare <jacob@parameter1.com>
2022-02-09 09:29:08 -0600

    Update root exports

--- packages/model/src/index.js
+++ packages/model/src/index.js
@@ -1,2 +1,6 @@
-export { Set, Map } from 'immutable';
+export { default as Joi, validate, validateAsync } from '@parameter1/joi';
+export { Set as ImmutableSet, Map as ImmutableMap } from 'immutable';
 export * from './base.js';
+export * from './entity.js';
+export * from './relationship.js';
+export * from './schema.js';

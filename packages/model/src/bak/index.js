3fdc215f5531e902065700e33f21be8319f72b7e
Jacob Bare <jacob@parameter1.com>
2022-02-08 22:18:56 -0600

    Delete converted instances

--- packages/model/src/bak/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Base } from './base.js';
-export * from './entity.js';
-export * from './relationship.js';
--- packages/model/src/bak/utils/entity-name.js
+++
@@ -1,9 +0,0 @@
-import inflector from '../inflector.js';
-import { attempt, string } from '../schema.js';
-
-const { pascal, singular } = inflector;
-
-export default (value) => {
-  const v = attempt(value, string().required());
-  return singular(pascal(v));
-};

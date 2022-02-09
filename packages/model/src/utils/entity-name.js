ac77e3611c69f9a898ca387ef2d306ec60cc5f72
Jacob Bare <jacob@parameter1.com>
2022-02-08 19:28:22 -0600

    Add docblock and optionally pass detination key

--- packages/model/src/utils/entity-name.js
+++ packages/model/src/utils/entity-name.js
@@ -1,7 +1,15 @@
 import { pascal, singular } from './inflector.js';
 import { attempt, string } from '../schema.js';
 
-export default (value) => {
-  const v = attempt(value, string().required());
+/**
+ * Formats the name of the entity. The value will be converted to PascalCase in
+ * singular form. As an example, `fruit-snacks` would become `FruitSnack`.
+ *
+ * @param {string} value The value to format
+ * @param {string} [key=name] The destination key
+ * @return {string} The PascalCased, singularized entity name
+ */
+export default (value, key = 'name') => {
+  const v = attempt(value, string().label(key).required());
   return singular(pascal(v));
 };

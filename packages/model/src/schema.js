abd2b5e3a4972f5f97be1f462aa94588ec549a42
Jacob Bare <jacob@parameter1.com>
2022-02-09 22:53:52 -0600

    Add `Schema.relationships` method

--- packages/model/src/schema.js
+++ packages/model/src/schema.js
@@ -1,24 +1,27 @@
 import { Map as ImmutableMap } from 'immutable';
 import { Base } from './base.js';
 import { Entity } from './entity.js';
+import { Relationship } from './relationship.js';
 import { PropTypes, attempt } from './prop-types.js';
 
 const { array, immutableMap, object } = PropTypes;
 
 const entityObjectType = object().instance(Entity);
+const relObjectType = object().instance(Relationship);
 
 export class Schema extends Base({
   $entities: ImmutableMap(),
+  $relationships: ImmutableMap(),
 }) {
   /**
    *
-   * @param {Entity[]} entities The entity definitions
+   * @param {Entity[]} values The entity definitions
    * @returns {this} The cloned instance
    */
-  entities(entities) {
-    attempt(entities, array().items(entityObjectType).required());
+  entities(values) {
+    attempt(values, array().items(entityObjectType).required());
     const $entities = this.getEntities().withMutations((map) => {
-      entities.forEach((entity) => {
+      values.forEach((entity) => {
         const name = entity.getName();
         if (map.has(name)) throw new Error(`An entity has already be added for \`${name}\``);
         map.set(name, entity);
@@ -29,10 +32,35 @@ export class Schema extends Base({
 
   /**
    *
+   * @param {Relationship[]} values The relationship definitions
+   * @returns {this} The cloned instance
+   */
+  relationships(values) {
+    attempt(values, array().items(relObjectType).required());
+    const $relationships = this.getRelationships().withMutations((map) => {
+      values.forEach((rel) => {
+        const key = rel.getKey();
+        if (map.has(key)) throw new Error(`A \`${rel.getHas().getType()}\` relationship using field \`${rel.getLocalField()}\` is already registered for \`${rel.getEntityName()}\``);
+        map.set(key, rel);
+      });
+    });
+    return this.set('$relationships', $relationships, { propType: immutableMap() });
+  }
+
+  /**
+   *
    * @returns {ImmutableMap<string, Entity>}
    */
   getEntities() {
     return this.get('$entities');
+  }
+
+  /**
+   *
+   * @returns {ImmutableMap<string, Relationship>}
+   */
+  getRelationships() {
+    return this.get('$relationships');
   }
 }
 
--- packages/model/test/schema.spec.js
+++ packages/model/test/schema.spec.js
@@ -3,6 +3,7 @@ import { describe, it } from 'mocha';
 import { expect } from 'chai';
 import { isMap } from 'immutable';
 import { entity } from '../src/entity.js';
+import { many, one } from '../src/relationship.js';
 import { createSchema } from '../src/schema.js';
 import common from './common.js';
 
@@ -45,6 +46,99 @@ describe('schema.js', () => {
       expect(isMap(entities)).to.equal(true);
       expect(entities.size).to.equal(3);
       expect([...entities.keys()]).to.deep.equal(['Application', 'Tag', 'User']);
+    });
+
+    /**
+     *
+     */
+    it('should add the entities using subsequent calls', () => {
+      const schema = createSchema()
+        .entities([entity('application')])
+        .entities([entity('Tag')])
+        .entities([entity('Users')]);
+
+      const entities = schema.getEntities();
+      expect(isMap(entities)).to.equal(true);
+      expect(entities.size).to.equal(3);
+      expect([...entities.keys()]).to.deep.equal(['Application', 'Tag', 'User']);
+    });
+  });
+
+  /**
+   *
+   */
+  describe('Schema.relationships', () => {
+    /**
+     *
+     */
+    it('should throw an error when invalid', () => {
+      common.testInvalidRequiredStrings((key) => {
+        createSchema().relationships(key);
+      });
+    });
+
+    /**
+     *
+     */
+    it('should error when the relationship already exists', () => {
+      expect(() => {
+        createSchema().relationships([
+          many('Applications').haveMany('Tags'),
+          many('Applications').haveMany('Tags'),
+        ]);
+      }).to.throw(Error, 'A `many` relationship using field `tag` is already registered for `Application`');
+
+      expect(() => {
+        createSchema().relationships([
+          many('Applications').haveMany('Tags'),
+          many('Applications').haveMany('Foos').as('tags'),
+        ]);
+      }).to.throw(Error, 'A `many` relationship using field `tag` is already registered for `Application`');
+
+      expect(() => {
+        createSchema().relationships([
+          one('Applications').haveMany('Tags'),
+          many('Applications').haveMany('Tags'),
+        ]);
+      }).to.throw(Error, 'A `many` relationship using field `tag` is already registered for `Application`');
+    });
+
+    /**
+     *
+     */
+    it('should add the relationships', () => {
+      const schema = createSchema().relationships([
+        many('Organizations').haveMany('Users').as('managers'),
+        many('Organizations').haveMany('Users'),
+        one('Organization').hasMany('Workspaces'),
+        many('UserEvents').haveOne('User'),
+      ]);
+      const relationships = schema.getRelationships();
+      expect(isMap(relationships)).to.equal(true);
+      expect([...relationships.keys()]).to.deep.equal([
+        'Organization.many.manager',
+        'Organization.many.user',
+        'Organization.many.workspace',
+        'UserEvent.one.user',
+      ]);
+    });
+
+    /**
+     *
+     */
+    it('should add the relationships using subsequent calls', () => {
+      const schema = createSchema()
+        .relationships([many('Organizations').haveMany('Users').as('managers')])
+        .relationships([one('Organization').hasMany('Workspaces')])
+        .relationships([many('UserEvents').haveOne('User')]);
+
+      const relationships = schema.getRelationships();
+      expect(isMap(relationships)).to.equal(true);
+      expect([...relationships.keys()]).to.deep.equal([
+        'Organization.many.manager',
+        'Organization.many.workspace',
+        'UserEvent.one.user',
+      ]);
     });
   });
 });

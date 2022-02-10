dd784fe8b01740b4d40e60d72c916fca26157650
Jacob Bare <jacob@parameter1.com>
2022-02-09 22:53:21 -0600

    Export schema from root

--- packages/model/src/index.js
+++ packages/model/src/index.js
@@ -4,5 +4,6 @@ export * from './base.js';
 export * from './entity.js';
 export * from './relationship.js';
 export * from './prop-types.js';
+export * from './schema.js';
 
 export { default as Inflector } from './utils/inflector.js';

bdc3ab8bb641762c627591c8aaae8ecd8ddbb431
Jacob Bare <jacob@parameter1.com>
2022-03-02 16:11:26 -0600

    Fix repo exports and db name

--- cli/src/repos.js
+++ cli/src/repos.js
@@ -1,5 +1,5 @@
-import { Repos } from '@parameter1/sso-db';
+import { ManagementRepos } from '@parameter1/sso-mongodb';
 import client from './mongodb.js';
 import { MONGO_DB_NAME, TOKEN_SECRET } from './env.js';
 
-export default new Repos({ client, dbName: MONGO_DB_NAME, tokenSecret: TOKEN_SECRET });
+export default new ManagementRepos({ client, dbName: MONGO_DB_NAME, tokenSecret: TOKEN_SECRET });

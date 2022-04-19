617ef9fa402be06e9399e03bb2244a56b8f3c4ae
Jacob Bare <jacob@parameter1.com>
2022-04-19 14:52:54 -0500

    Remove manager actions

--- cli/src/actions/index.js
+++ cli/src/actions/index.js
@@ -1,6 +1,5 @@
 import application from './application/index.js';
 import general from './general/index.js';
-import manager from './manager/index.js';
 import organization from './organization/index.js';
 import user from './user/index.js';
 import workspace from './workspace/index.js';
@@ -8,7 +7,6 @@ import workspace from './workspace/index.js';
 export default {
   application,
   general,
-  manager,
   organization,
   user,
   workspace,

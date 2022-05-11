48a83652b73ce5b0776d0282c13032524639ba75
Jacob Bare <jacob@parameter1.com>
2022-05-11 15:56:07 -0500

    Watch for changes to common graphql package

--- graphql/managed/gulpfile.js
+++ graphql/managed/gulpfile.js
@@ -2,5 +2,5 @@ import { gulpfile } from '@parameter1/gulp';
 
 gulpfile({
   entry: 'src/index.js',
-  watchPaths: ['src/**/*.js', '../../packages/mongodb/src/**/*.js'],
+  watchPaths: ['src/**/*.js', '../../packages/mongodb/src/**/*.js', '../common/src/**/*.js'],
 });

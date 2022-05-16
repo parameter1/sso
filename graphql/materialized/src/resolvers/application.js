0bff37f2724c781bd3e4f3efb2d5f25ba234519c
Jacob Bare <jacob@parameter1.com>
2022-05-16 11:01:08 -0500

    Pass correct key value

--- graphql/materialized/src/resolvers/application.js
+++ graphql/materialized/src/resolvers/application.js
@@ -7,8 +7,9 @@ export default {
      *
      */
     async applicationKeyExists(_, { input }, { repos }) {
+      console.log({ key: input.value });
       const doc = await repos.$('application').findOne({
-        query: { key: input.key },
+        query: { key: input.value },
         options: { projection: { _id: 1 } },
       });
       return Boolean(doc);

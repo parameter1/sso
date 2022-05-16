fb69db048a9ecea381f00468941551bc45022e0a
Jacob Bare <jacob@parameter1.com>
2022-05-16 11:10:51 -0500

    Remove console.log

--- graphql/materialized/src/resolvers/application.js
+++ graphql/materialized/src/resolvers/application.js
@@ -7,7 +7,6 @@ export default {
      *
      */
     async applicationKeyExists(_, { input }, { repos }) {
-      console.log({ key: input.value });
       const doc = await repos.$('application').findOne({
         query: { key: input.value },
         options: { projection: { _id: 1 } },

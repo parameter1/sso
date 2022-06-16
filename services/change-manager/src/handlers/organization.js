e8c42a7796d922c9af545d44dff90c33a2a5e910
Jacob Bare <jacob@parameter1.com>
2022-06-16 10:02:20 -0500

    Remove orgs from users on org delete

--- services/change-manager/src/handlers/organization.js
+++ services/change-manager/src/handlers/organization.js
@@ -10,8 +10,14 @@ export default {
     // @todo should this run through the PipelinedRepo.delete method?
     // @todo could also trigger soft-delete?
     return Promise.all([
+      // delete all workspaces associated with this org
       managedRepos.$('workspace').deleteMany({
         query: { '_edge.organization._id': _id },
+      }),
+      // remove the managed organization from all users
+      managedRepos.$('user').updateMany({
+        query: { '_connection.organization.edges._id': _id },
+        update: { $pull: { '_connection.organization.edges': { _id } } },
       }),
     ]);
   },

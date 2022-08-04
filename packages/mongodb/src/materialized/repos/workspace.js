29911dfcedf0576636e112fee18a878eacb6f3fc
Jacob Bare <jacob@parameter1.com>
2022-08-04 09:37:44 -0500

    Add `name.path` materialized workspace sort index

--- packages/mongodb/src/materialized/repos/workspace.js
+++ packages/mongodb/src/materialized/repos/workspace.js
@@ -13,6 +13,8 @@ export class MaterializedWorkspaceRepo extends BaseMaterializedRepo {
       indexes: [
         { key: { '_edge.organization.node._id': 1, '_edge.application.node._id': 1, key: 1 } },
         { key: { '_edge.application.node._id': 1 } },
+
+        { key: { 'name.path': 1, _id: 1 } },
       ],
     });
   }

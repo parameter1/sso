a26fe08111228ad8bae4f3d2d68ecb33273a8619
Jacob Bare <jacob@parameter1.com>
2022-08-04 09:37:02 -0500

    Port `getWorkspaceList` CLI util

---
+++ cli/src/cqrs/utils/get-workspace-list.js
@@ -0,0 +1,32 @@
+import { isFunction as isFn } from '@parameter1/utils';
+import { entityManager } from '../../mongodb.js';
+
+export default async ({
+  filter,
+  disabledWhen,
+  query,
+  projection,
+} = {}) => {
+  const repo = entityManager.getMaterializedRepo('workspace');
+  const pipeline = [
+    { $match: { _deleted: false, ...query } },
+    {
+      $project: {
+        ...projection,
+        name: 1,
+        namespace: 1,
+      },
+    },
+    { $sort: { 'name.path': 1, _id: 1 } },
+  ];
+  const cursor = await repo.aggregate({ pipeline });
+  const apps = await cursor.toArray();
+  return apps.filter((doc) => {
+    if (isFn(filter)) return filter(doc);
+    return true;
+  }).map((doc) => ({
+    name: `${doc.name.full} [${doc.namespace.default}]`,
+    value: doc,
+    disabled: isFn(disabledWhen) ? disabledWhen(doc) : false,
+  }));
+};
--- cli/src/cqrs/utils/index.js
+++ cli/src/cqrs/utils/index.js
@@ -1,4 +1,5 @@
 export { default as getAppList } from './get-app-list.js';
 export { default as getOrgList } from './get-org-list.js';
 export { default as getUserList } from './get-user-list.js';
+export { default as getWorkspaceList } from './get-workspace-list.js';
 export { waitUntilProcessed } from './wait-until-processed.js';

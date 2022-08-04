844d24ab995d54e7a617e94202e6943d2c80eabd
Jacob Bare <jacob@parameter1.com>
2022-08-04 12:50:05 -0500

    Materialized related workspace docs

--- services/materializer/src/index.js
+++ services/materializer/src/index.js
@@ -55,6 +55,33 @@ const { log } = console;
 
     // materialize
     const materialize = entityManager.materialize.bind(entityManager);
+
+    const materializeWorkspaceUsers = async ({ $match }) => {
+      const pipeline = [{
+        $match,
+      }, {
+        $group: { _id: null, workspaceIds: { $addToSet: '$_id' } },
+      }, {
+        $lookup: {
+          from: 'member/normalized',
+          localField: 'workspaceIds',
+          foreignField: '_id.workspace',
+          as: 'members',
+          pipeline: [{ $project: { _id: 0, userId: '$_id.user' } }],
+        },
+      }, {
+        $project: { userIds: { $map: { input: '$members', in: '$$this.userId' } } },
+      }];
+      // @todo the initial lookups to return the IDs doubles the overall materialization time
+      // determine if this can all be done in one $merge stage
+      const cursor = await entityManager.normalizedRepos.get('workspace').aggregate({ pipeline });
+      const result = await cursor.toArray();
+      if (!result) return null;
+      const [doc] = result;
+      if (!doc || !doc.userIds.length) return null;
+      return materialize({ entityType: 'user', $match: { _id: { $in: doc.userIds } } });
+    };
+
     const handlers = {
       /**
        *
@@ -62,12 +89,21 @@ const { log } = console;
       application: () => Promise.all([
         materialize({ entityType: 'application', $match: { _id: entityId } }),
         materialize({ entityType: 'workspace', $match: { appId: entityId } }),
+        materializeWorkspaceUsers({ $match: { appId: entityId } }),
       ]),
 
       /**
        *
        */
       manager: () => Promise.all([
+        materialize({ entityType: 'workspace', $match: { _id: entityId.workspace } }),
+        materialize({ entityType: 'user', $match: { _id: entityId.user } }),
+      ]),
+
+      /**
+       *
+       */
+      member: () => Promise.all([
         materialize({ entityType: 'organization', $match: { _id: entityId.org } }),
         materialize({ entityType: 'user', $match: { _id: entityId.user } }),
       ]),
@@ -75,31 +111,57 @@ const { log } = console;
       /**
        *
        */
-      organization: async () => {
-        const userIds = await entityManager.normalizedRepos.get('manager').distinct({
-          key: '_id.user',
-          query: { '_id.org': entityId },
-        });
-        return Promise.all([
-          materialize({ entityType: 'organization', $match: { _id: entityId } }),
-          materialize({ entityType: 'user', $match: { _id: { $in: userIds } } }),
-          materialize({ entityType: 'workspace', $match: { orgId: entityId } }),
-        ]);
-      },
+      organization: () => Promise.all([
+        materialize({ entityType: 'organization', $match: { _id: entityId } }),
+        materialize({ entityType: 'workspace', $match: { orgId: entityId } }),
+        materializeWorkspaceUsers({ $match: { orgId: entityId } }),
+        (async () => {
+          const userIds = await entityManager.normalizedRepos.get('manager').distinct({
+            key: '_id.user',
+            query: { '_id.org': entityId },
+          });
+          if (!userIds.length) return null;
+          return materialize({ entityType: 'user', $match: { _id: { $in: userIds } } });
+        })(),
+      ]),
 
       /**
        *
        */
-      user: async () => {
-        const orgIds = await entityManager.normalizedRepos.get('manager').distinct({
-          key: '_id.org',
-          query: { '_id.user': entityId },
-        });
-        return Promise.all([
-          materialize({ entityType: 'user', $match: { _id: entityId } }),
-          materialize({ entityType: 'organization', $match: { _id: { $in: orgIds } } }),
-        ]);
-      },
+      user: () => Promise.all([
+        materialize({ entityType: 'user', $match: { _id: entityId } }),
+        (async () => {
+          const orgIds = await entityManager.normalizedRepos.get('manager').distinct({
+            key: '_id.org',
+            query: { '_id.user': entityId },
+          });
+          if (!orgIds.length) return null;
+          return materialize({ entityType: 'organization', $match: { _id: { $in: orgIds } } });
+        })(),
+        (async () => {
+          const workspaceIds = await entityManager.normalizedRepos.get('member').distinct({
+            key: '_id.workspace',
+            query: { '_id.user': entityId },
+          });
+          if (!workspaceIds.length) return null;
+          return materialize({ entityType: 'workspace', $match: { _id: { $in: workspaceIds } } });
+        })(),
+      ]),
+
+      /**
+       *
+       */
+      workspace: () => Promise.all([
+        materialize({ entityType: 'workspace', $match: { _id: entityId } }),
+        (async () => {
+          const userIds = await entityManager.normalizedRepos.get('member').distinct({
+            key: '_id.user',
+            query: { '_id.workspace': entityId },
+          });
+          if (!userIds.length) return null;
+          return materialize({ entityType: 'user', $match: { _id: { $in: userIds } } });
+        })(),
+      ]),
     };
 
     const handler = handlers[entityType];

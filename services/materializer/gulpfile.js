5754285b0f403fc9a97d507dca4e6b7361c0f2af
Jacob Bare <jacob@parameter1.com>
2022-08-08 13:51:31 -0500

    Watch for changes to graphql redis pubsub package

--- services/materializer/gulpfile.js
+++ services/materializer/gulpfile.js
@@ -2,5 +2,9 @@ import gulpfile from '@parameter1/gulp/factory.js';
 
 gulpfile({
   entry: 'src/index.js',
-  watchPaths: ['src/**/*.js', '../../packages/mongodb/src/**/*.js'],
+  watchPaths: [
+    'src/**/*.js',
+    '../../packages/graphql-redis-pubsub/src/**/*.js',
+    '../../packages/mongodb/src/**/*.js',
+  ],
 });

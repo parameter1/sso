b7f2db98683b8e535427cd3df9853761dfd324a4
Jacob Bare <jacob@parameter1.com>
2022-08-08 13:53:53 -0500

    Add apollo websocket subscription client to app

--- app/src/apollo.js
+++ app/src/apollo.js
@@ -3,11 +3,25 @@ import {
   createHttpLink,
   InMemoryCache,
   ApolloLink,
+  split,
 } from '@apollo/client/core';
 import { onError } from '@apollo/client/link/error';
 import { setContext } from '@apollo/client/link/context';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { createClient } from 'graphql-ws';
 import { get } from 'object-path';
 import tokenStorage from './services/token-storage';
+
+const wsLink = new GraphQLWsLink(createClient({
+  url: import.meta.env.VITE_GRAPHQL_SUBSCRIPTIONS_URL,
+  connectionParams: async () => {
+    const token = get(tokenStorage.get(), 'value');
+    return {
+      ...(token && { authorization: `Bearer ${token}` }),
+    };
+  },
+}));
 
 const errorLink = onError(({ graphQLErrors: errors }) => {
   if (Array.isArray(errors) && errors.some((error) => get(error, 'extensions.code') === 'UNAUTHENTICATED' || get(error, 'extensions.status') === 401)) {
@@ -24,14 +38,25 @@ const contextLink = setContext(() => {
   return { headers };
 });
 
+const commandHttpLink = ApolloLink.from([
+  errorLink,
+  contextLink,
+  createHttpLink({
+    uri: import.meta.env.VITE_GRAPHQL_COMMAND_URL,
+  }),
+]);
+
 const command = new ApolloClient({
-  link: ApolloLink.from([
-    errorLink,
-    contextLink,
-    createHttpLink({
-      uri: import.meta.env.VITE_GRAPHQL_COMMAND_URL,
-    }),
-  ]),
+  link: split(
+    ({ query }) => {
+      const definition = getMainDefinition(query);
+      return (
+        definition.kind === 'OperationDefinition' && definition.operation === 'subscription'
+      );
+    },
+    wsLink,
+    commandHttpLink,
+  ),
   cache: new InMemoryCache(),
 });
 

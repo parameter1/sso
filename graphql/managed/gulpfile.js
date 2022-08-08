131bba240ffab29fd044cfff29e7dc15769e3709
Jacob Bare <jacob@parameter1.com>
2022-08-08 14:50:33 -0500

    Delete no longer used `managed` graphql api

--- graphql/managed/gulpfile.js
+++
@@ -1,6 +0,0 @@
-import { gulpfile } from '@parameter1/gulp';
-
-gulpfile({
-  entry: 'src/index.js',
-  watchPaths: ['src/**/*.js', '../../packages/mongodb/src/**/*.js', '../common/src/**/*.js'],
-});
--- graphql/managed/src/enums.js
+++
@@ -1,6 +0,0 @@
-import { paginationEnums, sortOrderEnums } from '@parameter1/graphql/features';
-
-export default {
-  ...paginationEnums,
-  ...sortOrderEnums,
-};

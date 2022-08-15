39ced16c9667cdbb97e2a44967fe7fc5b8766304
Jacob Bare <jacob@parameter1.com>
2022-08-15 10:17:18 -0500

    Create command processor service

---
+++ services/command-processor/src/index.js
@@ -0,0 +1,46 @@
+import { filterMongoURL } from '@parameter1/sso-mongodb';
+import { bootService } from '@parameter1/terminus';
+import { immediatelyThrow } from '@parameter1/utils';
+import process from 'process';
+
+import {
+  EXPOSED_HOST,
+  EXPOSED_PORT,
+  HOST,
+  PORT,
+} from './env.js';
+import pkg from '../package.js';
+import server from './server.js';
+import { mongodb } from './mongodb.js';
+
+process.on('unhandledRejection', immediatelyThrow);
+
+const { log } = console;
+
+bootService({
+  name: pkg.name,
+  version: pkg.version,
+  server,
+
+  host: HOST,
+  port: PORT,
+  exposedHost: EXPOSED_HOST,
+  exposedPort: EXPOSED_PORT,
+
+  onStart: async () => {
+    log('Connecting to MongoDB...');
+    const client = await mongodb.connect();
+    log(`MongoDB connected on ${filterMongoURL(client)}`);
+  },
+
+  onSignal: async () => {
+    log('Closing MongoDB...');
+    await mongodb.close();
+    log('MongoDB closed');
+  },
+
+  onHealthCheck: async () => {
+    await mongodb.ping({ id: pkg.name });
+    return true;
+  },
+}).catch(immediatelyThrow);

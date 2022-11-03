5b41cd73bb8e98c08ddd87a437bc136d00dc291f
Jacob Bare <jacob@parameter1.com>
2022-11-03 13:33:28 -0500

    Create `mongoSessionProp`

--- packages/mongodb/core/src/props.js
+++ packages/mongodb/core/src/props.js
@@ -1,6 +1,7 @@
-import { MongoClient } from 'mongodb';
+import { ClientSession, MongoClient } from 'mongodb';
 import { PropTypes } from '@parameter1/sso-prop-types-core';
 
 const { object } = PropTypes;
 
 export const mongoDBClientProp = object().instance(MongoClient).required();
+export const mongoSessionProp = object().instance(ClientSession);

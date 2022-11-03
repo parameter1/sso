0eeb2b806234d0fafd5b0aabb05401ee4786a486
Jacob Bare <jacob@parameter1.com>
2022-11-03 12:12:57 -0500

    Add `BaseCommandHandler.pushToStore` method

--- packages/mongodb/command/src/handlers/-base.js
+++ packages/mongodb/command/src/handlers/-base.js
@@ -1,34 +1,79 @@
-import { PropTypes, attempt } from '@parameter1/sso-prop-types-core';
-import { eventProps } from '@parameter1/sso-prop-types-event';
+import { PropTypes, attempt, validateAsync } from '@parameter1/sso-prop-types-core';
+import { eventProps, getEntityIdPropType } from '@parameter1/sso-prop-types-event';
 import { EventStore } from '@parameter1/sso-mongodb-event-store';
-import { SQSClient } from '@parameter1/sso-sqs';
+import { SQSClient, enqueueMessages } from '@parameter1/sso-sqs';
 
-const { object } = PropTypes;
+const { object, oneOrMany, url } = PropTypes;
 
 /**
- * @typedef BaseCommandHandlerConstructorParams
+ * @typedef CommandHandlerConstructorParamsSQS
+ * @property {SQSClient} client
+ * @property {string} url
+ *
+ * @typedef CommandHandlerConstructorParams
  * @property {string} entityType
- * @property {SQSClient} sqsClient
- * @property {EventStore} store
+ * @property {string} queueUrl
+ * @property {CommandHandlerConstructorParamsSQS} sqs
+ *
+ * @typedef {import("@parameter1/sso-mongodb-event-store").EventStoreDocument} EventStoreDocument
  */
 export class BaseCommandHandler {
   /**
    *
-   * @param {BaseCommandHandlerConstructorParams} params
+   * @param {CommandHandlerConstructorParams} params
    */
   constructor(params) {
-    /** @type {BaseCommandHandlerConstructorParams} */
-    const { entityType, store, sqsClient } = attempt(params, object({
+    /** @type {CommandHandlerConstructorParams} */
+    const { entityType, store, sqs } = attempt(params, object({
       entityType: eventProps.entityType.required(),
-      sqsClient: object().instance(SQSClient).required(),
+      sqs: object({
+        client: object().instance(SQSClient).required(),
+        url: url().required(),
+      }).required(),
       store: object().instance(EventStore).required(),
     }).required());
 
     /** @type {string} */
     this.entityType = entityType;
-    /** @type {SQSClient} */
-    this.sqsClient = sqsClient;
+    /** @type {CommandHandlerConstructorParamsSQS} */
+    this.sqs = sqs;
     /** @type {EventStore} */
     this.store = store;
+  }
+
+  /**
+   * @typedef CommandHandlerPushToStoreParams
+   * @property {EventStoreDocument|EventStoreDocument[]} events
+   *
+   * @param {CommandHandlerPushToStoreParams} params
+   */
+  async pushToStore(params) {
+    /** @type {CommandHandlerPushToStoreParams} */
+    const { events } = await validateAsync(object({
+      events: oneOrMany(object({
+        command: eventProps.command.required(),
+        entityId: getEntityIdPropType(this.entityType).required(),
+        date: eventProps.date,
+        omitFromHistory: eventProps.omitFromHistory,
+        omitFromModified: eventProps.omitFromModified,
+        values: eventProps.values,
+        userId: eventProps.userId,
+      }).required()).required(),
+    }).required().label('pushToStore'), params);
+
+    const session = await this.store.mongo.startSession();
+
+    try {
+      await session.withTransaction(async (activeSession) => {
+        const results = await this.store.push(this.entityType, { events, session: activeSession });
+        await enqueueMessages({
+          bodies: results,
+          queueUrl: this.sqs.url,
+          sqsClient: this.sqs.client,
+        });
+      });
+    } finally {
+      await session.endSession();
+    }
   }
 }

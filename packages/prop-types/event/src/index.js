8d1902d92a89c725c76aafe29ff4ddaa4aa168a7
Jacob Bare <jacob@parameter1.com>
2022-11-03 10:38:31 -0500

    Create event prop types package

---
+++ packages/prop-types/event/src/index.js
@@ -0,0 +1,23 @@
+import { PropTypes } from '@parameter1/sso-prop-types-core';
+import { EntityTypes } from '@parameter1/sso-entity-types';
+
+const {
+  any,
+  boolean,
+  date,
+  object,
+  objectId,
+  string,
+} = PropTypes;
+
+export const eventProps = {
+  _id: objectId(),
+  command: string().uppercase().pattern(/^[A-Z_]+$/),
+  entityId: any().disallow(null, ''),
+  entityType: string().valid(...EntityTypes.getKeys()),
+  date: date().allow('$$NOW'),
+  omitFromHistory: boolean(),
+  omitFromModified: boolean(),
+  values: object(),
+  userId: objectId().allow(null),
+};

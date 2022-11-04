6e213928ef70b58b5f6ce9fd5f970a7f9325b8d7
Jacob Bare <jacob@parameter1.com>
2022-11-04 09:38:04 -0500

    Require at least one letter in app name and key

--- packages/mongodb/command/src/props/application.js
+++ packages/mongodb/command/src/props/application.js
@@ -5,7 +5,7 @@ const { array, string, slug } = PropTypes;
 
 export const applicationProps = {
   id: getEntityIdPropType('application'),
-  key: slug().pattern(/[a-z0-9]/i).min(2),
-  name: string().pattern(/[a-z0-9]/i).min(2),
+  key: slug().pattern(/[a-z]/i).min(2),
+  name: string().pattern(/[a-z]/i).min(2),
   roles: array().items(string().required()),
 };

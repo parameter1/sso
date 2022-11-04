833959d7fd03be7ad0c2a866df1c4efd755b8589
Jacob Bare <jacob@parameter1.com>
2022-11-04 09:24:46 -0500

    Rework SQS enqueue to support attributes
    And add better failed message handling

--- packages/sqs/src/create-message-batch-commands.js
+++ packages/sqs/src/create-message-batch-commands.js
@@ -2,28 +2,45 @@ import { SendMessageBatchCommand } from '@aws-sdk/client-sqs';
 import { PropTypes, validateAsync } from '@parameter1/sso-prop-types-core';
 import { EJSON } from 'bson';
 import { chunkArray } from '@parameter1/utils';
+import { attributeValueFieldMap, sqsMessagesSchema } from './schema.js';
 
-const { array, object, url } = PropTypes;
+const { object, url } = PropTypes;
 
 /**
+ * @typedef {import("./types").SQSMessageSchema} SQSMessageSchema
  *
- * @param {object} params
- * @param {object[]} params.bodies
- * @param {string} params.queueUrl
+ * @typedef CreateMessageBatchCommandParams
+ * @property {SQSMessageSchema[]} messages
+ * @property {string} queueUrl
+ *
+ * @param {CreateMessageBatchCommandParams} params
  * @returns {Promise<Array<object[]>>}
  */
 export async function createMessageBatchCommands(params) {
-  const { bodies, queueUrl } = await validateAsync(object({
-    bodies: array().items(object().required()).required(),
+  /** @type {CreateMessageBatchCommandParams} */
+  const { messages, queueUrl } = await validateAsync(object({
+    messages: sqsMessagesSchema,
     queueUrl: url().required(),
   }).required(), params);
 
-  const chunks = chunkArray(bodies, 10);
+  const chunks = chunkArray(messages, 10);
   return chunks.map((objs, chunkIndex) => new SendMessageBatchCommand({
     QueueUrl: queueUrl,
-    Entries: objs.map((body, objIndex) => ({
-      Id: `${chunkIndex}_${objIndex}`,
-      MessageBody: EJSON.stringify(body),
-    })),
+    Entries: objs.map((message, objIndex) => {
+      const { attributes } = message;
+      return {
+        Id: `${chunkIndex}_${objIndex}`,
+        MessageBody: EJSON.stringify(message.body),
+        ...(attributes.length && {
+          MessageAttributes: attributes.reduce((o, { name, type, value }) => {
+            const valueField = attributeValueFieldMap.get(type);
+            return {
+              ...o,
+              [name]: { DataType: type, [valueField]: value },
+            };
+          }, {}),
+        }),
+      };
+    }),
   }));
 }
--- packages/sqs/src/enqueue-messages.js
+++ packages/sqs/src/enqueue-messages.js
@@ -1,24 +1,39 @@
 import { SQSClient } from '@aws-sdk/client-sqs';
 import { PropTypes, validateAsync } from '@parameter1/sso-prop-types-core';
 import { createMessageBatchCommands } from './create-message-batch-commands.js';
+import { sqsMessagesSchema } from './schema.js';
 
-const { array, object, url } = PropTypes;
+const { object, url } = PropTypes;
 
 /**
+ * @typedef {import("./types").SQSMessageSchema} SQSMessageSchema
  *
- * @param {object} params
- * @param {object[]} params.bodies
- * @param {string} params.queueUrl
- * @param {SQSClient} params.sqsClient
- * @returns {Promise<object[]>}
+ * @typedef EnqueueMessagesParams
+ * @property {SQSMessageSchema[]} messages
+ * @property {string} queueUrl
+ * @property {SQSClient} sqsClient
+ *
+ * @param {EnqueueMessagesParams} params
+ * @returns {Promise<Array<object[]>>}
  */
 export async function enqueueMessages(params) {
-  const { bodies, queueUrl, sqsClient } = await validateAsync(object({
-    bodies: array().items(object().required()).required(),
+  /** @type {EnqueueMessagesParams} */
+  const { messages, queueUrl, sqsClient } = await validateAsync(object({
+    messages: sqsMessagesSchema,
     queueUrl: url().required(),
     sqsClient: object().instance(SQSClient).required(),
   }).required(), params);
 
-  const commands = await createMessageBatchCommands({ bodies, queueUrl });
-  return Promise.all(commands.map((command) => sqsClient.send(command)));
+  const commands = await createMessageBatchCommands({ messages, queueUrl });
+  return Promise.all(commands.map(async (command) => {
+    const response = await sqsClient.send(command);
+    const { Failed } = response;
+    if (Failed && Failed.length) {
+      const [first] = Failed;
+      const error = new Error(`SQS message enqueue encountered ${Failed.length} error(s). First encountered: ${first.Message} (${first.Code})`);
+      error.failed = Failed;
+      throw error;
+    }
+    return response;
+  }));
 }

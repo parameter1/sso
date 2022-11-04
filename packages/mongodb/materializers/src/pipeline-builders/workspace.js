6b6156b0332199dfc4d6a85c8a2f22980b8e68d7
Jacob Bare <jacob@parameter1.com>
2022-11-04 13:43:44 -0500

    Create pipeline builders for orgs, users, and workspaces

---
+++ packages/mongodb/materializers/src/pipeline-builders/workspace.js
@@ -0,0 +1,122 @@
+import { PipelineBuilder } from './-root.js';
+import {
+  fullWorkspace,
+  partialApplication,
+  partialOrganization,
+  partialUser,
+} from './-projections.js';
+
+export class WorkspacePipelineBuilder extends PipelineBuilder {
+  constructor() {
+    super({ entityType: 'workspace' });
+  }
+
+  /**
+   *
+   * @param {object} [$match]
+   * @returns {object[]}
+   */
+  buildPipeline($match) {
+    const stages = [];
+    // application
+    stages.push(...WorkspacePipelineBuilder.applicationStages());
+    // organization
+    stages.push(...WorkspacePipelineBuilder.organizationStages());
+    // members
+    stages.push({
+      $lookup: {
+        from: 'member/normalized',
+        localField: '_id',
+        foreignField: '_id.workspace',
+        as: '_connection.member.edges',
+        pipeline: [
+          // do not include deleted members
+          { $match: { $expr: { $eq: ['$_deleted', false] } } },
+          // drop history
+          { $project: { _history: 0 } },
+          // lookup the user
+          {
+            $lookup: {
+              from: 'user/normalized',
+              localField: '_id.user',
+              foreignField: '_id',
+              as: 'node',
+              pipeline: [
+                // do not include deleted users
+                { $match: { $expr: { $eq: ['$_deleted', false] } } },
+                { $project: partialUser() },
+              ],
+            },
+          },
+          // flatten the user into a single object
+          // and discard when no active user is found (i.e. do not preserve)
+          { $unwind: { path: '$node', preserveNullAndEmptyArrays: false } },
+        ],
+      },
+    }, {
+      $set: {
+        '_connection.member.edges': {
+          $map: {
+            // set edge fields
+            input: '$_connection.member.edges',
+            in: ['_meta', 'node', 'role'].reduce((o, key) => ({ ...o, [key]: `$$this.${key}` }), {}),
+          },
+        },
+      },
+    });
+    stages.push({ $project: fullWorkspace() });
+    return this.build({ $match, stages });
+  }
+
+  static applicationStages() {
+    return [
+      {
+        $lookup: {
+          from: 'application/normalized',
+          localField: 'appId',
+          foreignField: '_id',
+          as: '_edge.application.node',
+          pipeline: [
+            { $project: partialApplication() },
+          ],
+        },
+      }, {
+        // flatten the app into a single object
+        $unwind: { path: '$_edge.application.node', preserveNullAndEmptyArrays: true },
+      }, {
+        $set: {
+          // mark the workspace as deleted if the app is deleted.
+          _deleted: {
+            $cond: [{ $eq: ['$_edge.application.node._deleted', true] }, true, '$_deleted'],
+          },
+        },
+      },
+    ];
+  }
+
+  static organizationStages() {
+    return [
+      {
+        $lookup: {
+          from: 'organization/normalized',
+          localField: 'orgId',
+          foreignField: '_id',
+          as: '_edge.organization.node',
+          pipeline: [
+            { $project: partialOrganization() },
+          ],
+        },
+      }, {
+        // flatten the org into a single object
+        $unwind: { path: '$_edge.organization.node', preserveNullAndEmptyArrays: true },
+      }, {
+        $set: {
+          // mark the workspace as deleted if the org is deleted.
+          _deleted: {
+            $cond: [{ $eq: ['$_edge.organization._deleted', true] }, true, '$_deleted'],
+          },
+        },
+      },
+    ];
+  }
+}

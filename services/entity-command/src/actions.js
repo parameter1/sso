d8b1b3d9ef04f2a8c70824664c8edf0655583599
Jacob Bare <jacob@parameter1.com>
2022-11-04 09:38:14 -0500

    Create `organization` entity command actions

--- services/entity-command/src/actions.js
+++ services/entity-command/src/actions.js
@@ -1,6 +1,8 @@
 import application from './actions/application.js';
+import organization from './actions/organization.js';
 
 export default {
   application,
+  organization,
   ping: () => 'pong',
 };

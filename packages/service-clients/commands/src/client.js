838b7f5f2b6a7ad126e3f2197ad749d40c220f03
Jacob Bare <jacob@parameter1.com>
2022-11-07 13:59:29 -0600

    remove unused service clients packages

--- packages/service-clients/commands/src/client.js
+++
@@ -1,9 +0,0 @@
-import { EJSONClient } from '@parameter1/micro-ejson';
-
-export class CommandServiceClient extends EJSONClient {
-  constructor(...args) {
-    super(...args);
-
-    this.foo = 'nar';
-  }
-}

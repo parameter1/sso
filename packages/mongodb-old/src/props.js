09bd003ec973539202a3e8b735b27d42a77b8868
Jacob Bare <jacob@parameter1.com>
2022-11-06 22:56:31 -0600

    Remove old mongodb package

--- packages/mongodb-old/src/props.js
+++
@@ -1,7 +0,0 @@
-import { MongoDBClient } from '@parameter1/mongodb';
-import { PropTypes } from '@parameter1/sso-prop-types-core';
-
-const { object, string } = PropTypes;
-
-export const mongoDBClientProp = object().instance(MongoDBClient).required();
-export const entityTypeProp = string().lowercase().pattern(/^[a-z-]+$/);

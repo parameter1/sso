3c1d5bba0c4ac902023bdc61da6795150d25b370
Jacob Bare <jacob@parameter1.com>
2022-11-08 10:59:14 -0600

    Create p1 wide logo component

---
+++ app/src/components/logos/parameter1-wide.vue
@@ -0,0 +1,12 @@
+<template>
+  <img
+    src="https://img.parameter1.com/www/p1-logo-glass-vinyl.svg"
+    alt="Parameter1 Logo"
+  >
+</template>
+
+<script>
+export default {
+  name: 'LogoParameter1Wide',
+};
+</script>

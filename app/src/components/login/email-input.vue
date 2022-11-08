bd6759b04ea27f1e34ad307c1418680910688303
Jacob Bare <jacob@parameter1.com>
2022-11-08 08:54:33 -0600

    Use generic input field for login email input

--- app/src/components/login/email-input.vue
+++ app/src/components/login/email-input.vue
@@ -1,23 +1,29 @@
 <template>
-  <input
+  <input-field
     id="email"
     type="email"
     autocomplete="email"
-    :value="modelValue"
+    :model-value="modelValue"
     class="block w-full appearance-none rounded-md
     border border-slate-300 px-3 py-2 placeholder-slate-400 shadow-sm
     caret-blue-600
     focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
     required
     @input="$emit('update:modelValue', $event.target.value)"
-  >
+  />
 </template>
 
 <script>
+import InputField from '../fields/input.vue';
+
 export default {
   name: 'LoginEmailInput',
 
   emits: ['update:modelValue'],
+
+  components: {
+    InputField,
+  },
 
   props: {
     modelValue: {

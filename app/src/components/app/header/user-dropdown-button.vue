7eff224441ed45835c3e3f2ef90f8e041ce1be5b
Jacob Bare <jacob@parameter1.com>
2022-11-09 12:39:48 -0600

    Add user dropdown button focus

--- app/src/components/app/header/user-dropdown-button.vue
+++ app/src/components/app/header/user-dropdown-button.vue
@@ -5,13 +5,13 @@
       small ? 'text-sm' : 'text-base',
       'block w-full text-slate-700 hover:bg-slate-50 hover:text-slate-900',
       'group flex items-center px-2 py-2 font-base',
-      'focus:outline-none '
+      'focus:outline-none focus:bg-slate-50 focus:text-slate-900'
     ]"
   >
     <component
       :is="icon"
       :class="[
-        'text-slate-400 group-hover:text-blue-500',
+        'text-slate-400 group-hover:text-blue-500 group-focus:text-blue-500',
         'mr-2 h-5 w-5',
       ]"
       aria-hidden="true"

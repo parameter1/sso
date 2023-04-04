d4fc4f3a7eb87edf46f7c723b75b5bd77fa8aed6
Jacob Bare <jacob@parameter1.com>
2023-04-04 15:29:58 -0500

    Convert appIds to object IDs

--- cli/src/sync/sources/identity-x.js
+++ cli/src/sync/sources/identity-x.js
@@ -6,17 +6,18 @@ export class IdentityXSource extends AbstractSource {
    *
    * @param {object} params
    * @param {string} params.orgId The IdentityX organization ID
-   * @param {string[]} [params.appIds] Application IDs to use. An empty values signifies all.
+   * @param {string[]} [params.appIds=[]] Application IDs to use. An empty values signifies all.
    */
   constructor({ orgId, appIds = [] }) {
     if (!/^[a-f0-9]{24}$/.test(`${orgId}`)) throw new Error(`Invalid IdentityX org ID: ${orgId}`);
 
     super({ kind: 'identity-x', key: `${orgId}` });
     this.org = new ObjectId(`${orgId}`);
+    /** @type {ObjectId[]} */
     this.appIds = [...appIds.reduce((map, appId) => {
       const key = `${appId}`;
       if (!/^[a-f0-9]{24}$/.test(`${key}`)) throw new Error(`Invalid IdentityX app ID: ${key}`);
-      map.set(key, appId);
+      map.set(key, new ObjectId(appId));
       return map;
     }, new Map()).values()];
   }

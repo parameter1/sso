d3a6e743ccb0855ddc851c9c6a47bd4cd0930a1c
Jacob Bare <jacob@parameter1.com>
2023-04-04 15:30:06 -0500

    Add `publisherIds` to `NativeXSource`

--- cli/src/sync/sources/native-x.js
+++ cli/src/sync/sources/native-x.js
@@ -1,3 +1,4 @@
+import { ObjectId } from '@parameter1/mongodb-core';
 import { AbstractSource } from './-abstract.js';
 
 export class NativeXSource extends AbstractSource {
@@ -5,11 +6,19 @@ export class NativeXSource extends AbstractSource {
    *
    * @param {object} params
    * @param {string} params.tenant The NativeX tenant key, e.g. `acbm`
+   * @param {string[]} [params.publisherIds=[]] Publisher IDs to use. An empty values signifies all.
    */
-  constructor({ tenant }) {
+  constructor({ tenant, publisherIds = [] }) {
     if (!/^[a-z0-9]{2,}$/.test(tenant)) throw new Error(`Invalid NativeX tenant key: ${tenant}`);
 
     super({ kind: 'native-x', key: tenant });
     this.tenant = tenant;
+    /** @type {ObjectId[]} */
+    this.publisherIds = [...publisherIds.reduce((map, publisherId) => {
+      const key = `${publisherId}`;
+      if (!/^[a-f0-9]{24}$/.test(`${key}`)) throw new Error(`Invalid NativeX publisher ID: ${key}`);
+      map.set(key, new ObjectId(publisherId));
+      return map;
+    }, new Map()).values()];
   }
 }

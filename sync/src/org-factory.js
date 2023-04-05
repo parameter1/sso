643b452976cbc5e49da60863103465b0c383a9fd
Jacob Bare <jacob@parameter1.com>
2023-04-05 13:05:46 -0500

    Add abmedia website value

--- sync/src/org-factory.js
+++ sync/src/org-factory.js
@@ -14,6 +14,7 @@ export function createOrgManager() {
       new Organization({
         key: 'abmedia',
         name: 'AB Media',
+        website: 'http://www.abmedia.biz/',
         workspaces: [
           new Workspace({
             sources: [

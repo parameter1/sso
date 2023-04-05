b6f3028a21529cada8d408f8942aec6ed0cd0e19
Jacob Bare <jacob@parameter1.com>
2023-04-05 13:06:34 -0500

    Create initial org sync

--- sync/src/index.js
+++ sync/src/index.js
@@ -1,5 +1,41 @@
-import { inspect } from 'util';
+import { filterMongoURL } from '@parameter1/mongodb-core';
+import { immediatelyThrow } from '@parameter1/utils';
+import { commands } from './service-clients.js';
 import { createOrgManager } from './org-factory.js';
+import { mongo, materializer, normalizer } from './mongodb.js';
+
+process.on('unhandledRejection', immediatelyThrow);
+
+const { log } = console;
 
 const orgManager = createOrgManager();
-console.log(inspect(orgManager, { colors: true, depth: null }));
+
+(async () => {
+  log('> Connecting to MongoDB...');
+  await mongo.connect();
+  log(`> MongoDB connection to ${filterMongoURL(mongo)}`);
+
+  // upsert the orgs
+  const input = [...orgManager.orgs.values()].reduce((arr, org) => {
+    arr.push({ values: { key: org.key, name: org.name, website: org.website } });
+    return arr;
+  }, []);
+  const results = await commands.request('organization.create', {
+    input,
+    upsert: true,
+  });
+
+  const orgIds = [];
+  results.forEach(({ entityId, values }) => {
+    orgIds.push(entityId);
+    orgManager.get(values.key).setId(entityId);
+  }, new Map());
+
+  // then normalize/materialize.
+  await normalizer.normalize({ entityIds: orgIds, entityType: 'organization' });
+  await materializer.materialize('organization', { entityIds: orgIds });
+
+  log('> Closing to MongoDB...');
+  await mongo.close();
+  log('> MongoDB closed');
+})().catch(immediatelyThrow);

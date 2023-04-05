3604d4fa2dc95e953165f9689f12bfe518a23200
Jacob Bare <jacob@parameter1.com>
2023-04-05 13:36:00 -0500

    Import workspaces

--- sync/src/index.js
+++ sync/src/index.js
@@ -2,7 +2,12 @@ import { filterMongoURL } from '@parameter1/mongodb-core';
 import { immediatelyThrow } from '@parameter1/utils';
 import { commands } from './service-clients.js';
 import { createOrgManager } from './org-factory.js';
-import { mongo, materializer, normalizer } from './mongodb.js';
+import {
+  mongo,
+  materializedRepoManager,
+  materializer,
+  normalizer,
+} from './mongodb.js';
 
 process.on('unhandledRejection', immediatelyThrow);
 
@@ -15,13 +20,19 @@ const orgManager = createOrgManager();
   await mongo.connect();
   log(`> MongoDB connection to ${filterMongoURL(mongo)}`);
 
+  // load the app
+  const app = await materializedRepoManager.get('application').findByKey('mindful', {
+    projection: { _id: 1 },
+  });
+  if (!app) throw new Error('Unable to load the mindful app');
+
   // upsert the orgs
-  const input = [...orgManager.orgs.values()].reduce((arr, org) => {
+  const orgInput = [...orgManager.orgs.values()].reduce((arr, org) => {
     arr.push({ values: { key: org.key, name: org.name, website: org.website } });
     return arr;
   }, []);
   const results = await commands.request('organization.create', {
-    input,
+    input: orgInput,
     upsert: true,
   });
 
@@ -34,6 +45,34 @@ const orgManager = createOrgManager();
   // then normalize/materialize.
   await normalizer.normalize({ entityIds: orgIds, entityType: 'organization' });
   await materializer.materialize('organization', { entityIds: orgIds });
+
+  // upsert the workspaces
+  const workspaceInput = [];
+  orgManager.orgs.forEach((org) => {
+    org.workspaces.forEach((workspace) => {
+      workspaceInput.push({
+        values: {
+          appId: app._id,
+          orgId: org._id,
+          key: workspace.key,
+          name: workspace.name,
+        },
+      });
+    });
+  });
+  const workspaceResults = await commands.request('workspace.create', {
+    input: workspaceInput,
+    upsert: true,
+  });
+
+  const workspaceIds = [];
+  workspaceResults.forEach(({ entityId }) => {
+    workspaceIds.push(entityId);
+  }, new Map());
+
+  // then normalize/materialize.
+  await normalizer.normalize({ entityIds: workspaceIds, entityType: 'workspace' });
+  await materializer.materialize('workspace', { entityIds: workspaceIds });
 
   log('> Closing to MongoDB...');
   await mongo.close();

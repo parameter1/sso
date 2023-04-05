386d3142e20941a3d64f9050d55557f508dde706
Jacob Bare <jacob@parameter1.com>
2023-04-05 13:34:58 -0500

    Support upserting workspaces

--- packages/mongodb/command/src/workspace/commands.js
+++ packages/mongodb/command/src/workspace/commands.js
@@ -1,10 +1,11 @@
+import { EJSON } from '@parameter1/mongodb-bson';
 import { PropTypes, attempt, validateAsync } from '@parameter1/sso-prop-types-core';
 import { EventStore } from '@parameter1/sso-mongodb-event-store';
 import { sluggify } from '@parameter1/slug';
 
 import { changeWorkspaceName, createWorkspace } from './schema.js';
 
-const { array, object } = PropTypes;
+const { array, boolean, object } = PropTypes;
 
 /**
  * @typedef {import("../types").EventStoreResult} EventStoreResult
@@ -62,10 +63,12 @@ export class WorkspaceCommands {
    */
   async create(params) {
     /** @type {CreateParams}  */
-    const { input } = await validateAsync(object({
+    const { input, upsert } = await validateAsync(object({
       input: array().items(createWorkspace).required(),
+      upsert: boolean().default(false),
     }).required().label('workspace.create'), params);
 
+    const { entityType } = this;
     const session = this.store.startSession();
     try {
       let results;
@@ -74,21 +77,42 @@ export class WorkspaceCommands {
         await this.store.reserve({
           input: input.map((o) => ({
             entityId: o.entityId,
-            entityType: this.entityType,
+            entityType,
             key: 'app_org_key',
             value: `${o.values.appId}_${o.values.orgId}_${o.values.key}`,
+            upsert,
           })),
           session: activeSession,
         });
 
-        results = await this.store.executeCreate({
-          entityType: this.entityType,
-          input: input.map(({ values, ...rest }) => ({
-            ...rest,
-            values: { ...values, slug: sluggify(values.name) },
-          })),
-          session: activeSession,
-        });
+        const toPush = input.map(({ values, ...rest }) => ({
+          ...rest,
+          values: { ...values, slug: sluggify(values.name) },
+          ...(upsert && { upsertOn: ['appId', 'orgId', 'key'] }),
+        }));
+
+        results = upsert
+          ? await this.store.upsert({ entityType, events: toPush, session: activeSession })
+          : await this.store.executeCreate({ entityType, input: toPush, session: activeSession });
+
+        if (upsert) {
+          // when upserting, release any reservations from entities that have since been deleted
+          const entityIds = results.map(({ entityId }) => entityId);
+          const states = await this.store.getEntityStatesFor({
+            entityType,
+            entityIds,
+            session: activeSession,
+          });
+
+          const toRelease = [];
+          states.forEach((state, encoded) => {
+            const entityId = EJSON.parse(encoded);
+            if (state === 'DELETED') toRelease.push({ entityId, entityType, key: 'app_org_key' });
+          });
+          if (toRelease.length) {
+            await this.store.release({ input: toRelease, session: activeSession });
+          }
+        }
       });
       return results;
     } finally {

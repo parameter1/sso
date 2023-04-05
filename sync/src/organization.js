85f4a30152fe013f6caef61e5f6657fa7e42aa38
Jacob Bare <jacob@parameter1.com>
2023-04-05 13:05:58 -0500

    Add website and _id support to org class

--- sync/src/organization.js
+++ sync/src/organization.js
@@ -9,13 +9,20 @@ export class Organization {
    * @param {object} params
    * @param {string} params.key
    * @param {string} params.name
+   * @param {string} [params.website]
    * @param {Workspace[]} params.workspaces
    */
-  constructor({ key, name, workspaces = [] }) {
+  constructor({
+    key,
+    name,
+    website,
+    workspaces = [],
+  }) {
     if (!/^[a-z0-9][a-z0-9-]{0,}[a-z0-9]{1,}$/.test(key)) throw new Error(`Invalid organization key: ${key}`);
     if (!name) throw new Error(`Invalid organization name: ${name}`);
     this.key = key;
     this.name = name;
+    this.website = website;
 
     /** @type {Map<string, Workspace>} */
     this.workspaces = new Map();
@@ -32,5 +39,17 @@ export class Organization {
     if (this.workspaces.has(workspace.key)) throw new Error(`The workspace ${workspace.key} already exists for org ${this.key}`);
     this.workspaces.set(workspace.key, workspace);
     return this;
+  }
+
+  hasId() {
+    return Boolean(this._id);
+  }
+
+  /**
+   *
+   * @param {import("@parameter1/mongodb-core").ObjectId} _id
+   */
+  setId(_id) {
+    this._id = _id;
   }
 }

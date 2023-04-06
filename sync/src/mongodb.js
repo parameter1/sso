3f34d175c71cda249372a0137f381cc4aad1e5c8
Jacob Bare <jacob@parameter1.com>
2023-04-06 10:22:24 -0500

    Add basedb and mongos for aquaria, tauron and virgon

--- sync/src/mongodb.js
+++ sync/src/mongodb.js
@@ -1,13 +1,33 @@
 import { MongoClient } from '@parameter1/mongodb-core';
+import basedb from '@parameter1/base-cms-db';
 import { MaterializedRepoManager } from '@parameter1/sso-mongodb-materialized';
 import { NormalizedRepoManager } from '@parameter1/sso-mongodb-normalized';
 import { Materializer } from '@parameter1/sso-mongodb-materializers';
 import { Normalizer } from '@parameter1/sso-mongodb-normalizers';
 
-import { MONGO_URL } from './env.js';
+import {
+  MONGO_URL,
+  MONGO_URL_AQUARIA,
+  MONGO_URL_TAURON,
+  MONGO_URL_VIRGON,
+} from './env.js';
 import pkg from '../package.js';
 
+const { createMongoClient } = basedb;
+
 export const mongo = new MongoClient(MONGO_URL, {
+  appname: `${pkg.name} v${pkg.version}`,
+});
+
+export const aquaria = new MongoClient(MONGO_URL_AQUARIA, {
+  appname: `${pkg.name} v${pkg.version}`,
+});
+
+export const tauron = createMongoClient(MONGO_URL_TAURON, {
+  appname: `${pkg.name} v${pkg.version}`,
+});
+
+export const virgon = createMongoClient(MONGO_URL_VIRGON, {
   appname: `${pkg.name} v${pkg.version}`,
 });
 
